1c3950e320bee919f0805f7b48f465b57cf20e28
Andres Astra <andieroid@arizona.edu>
2024-01-31 10:16:10 -0500

    Completed Exercise 2

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,4 @@
 class Product < ApplicationRecord
+    validates :name, presence: true, uniqueness: true, length: { minimum: 5, maximum: 50 }
+    validates :price, presence: true, numericality: { greater_than: 0 }
 end
